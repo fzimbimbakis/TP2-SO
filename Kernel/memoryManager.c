29717d950ac967b017d4a2594f61100e2adeacf1
bsquillari <bsquillari@itba.edu.ar>
2021-09-27 16:00:39 -0300

    Archivos de MM. No funciona el alloc y el free esta sin testear.

---
+++ Kernel/memoryManager.c
@@ -0,0 +1,107 @@
+#include "memoryManager.h"
+
+typedef long Align;
+// For alignment for long values ???????
+
+union header {
+	struct
+	{
+		union header * ptr;
+		unsigned size;
+	} s;
+	Align x;
+} ;
+typedef union header Header;
+
+static Header * freeList = 0x600000;
+
+void initial(){
+	Header base;
+	base.s.ptr = 0;
+	base.s.size = (END_MEM - BEGIN_MEM)/sizeof(Header);
+	*freeList = base;
+}
+
+void * alloc( unsigned nbytes){
+	Header * Hpointer, * prevHpointer;
+	unsigned nunits;
+
+	nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
+	Hpointer = freeList;
+	prevHpointer = freeList;
+	if(freeList==NULL)
+		return NULL;
+	do
+	{
+		if(Hpointer->s.size >= nunits){
+			if(Hpointer->s.size == nunits){
+
+				if(prevHpointer != freeList)
+				prevHpointer->s.ptr = Hpointer->s.ptr;
+				else freeList = Hpointer->s.ptr;
+
+			} else {
+
+				Hpointer->s.size -= nunits;
+				Hpointer += Hpointer->s.size;			
+				Hpointer->s.size = nunits;
+				
+			}
+			return (void *) (Hpointer+1);
+		}
+		if(Hpointer->s.ptr==NULL)
+			return NULL;
+		prevHpointer = Hpointer;
+		Hpointer = Hpointer->s.ptr;
+	} while (1);
+
+}
+
+
+void free(void * block){
+	Header * blockPointer;
+
+	blockPointer = (Header *) block - 1; // point to block header
+
+	// No other free space
+	if(freeList==NULL){
+		freeList = blockPointer;
+		return;
+		}
+
+	// Block before freeList
+	if(blockPointer < freeList){
+		if(blockPointer + blockPointer->s.size == freeList){
+			blockPointer->s.ptr=freeList->s.ptr;
+			blockPointer->s.size+=freeList->s.size;
+			freeList = blockPointer;
+		}else{
+			blockPointer->s.ptr = freeList;
+			freeList = blockPointer;
+		}
+		return;
+	}
+
+	Header * freeListPointer = freeList;
+
+	// Block after freeList
+	for (;!(blockPointer>freeListPointer && (freeListPointer->s.ptr==NULL || blockPointer<freeListPointer->s.ptr)); 
+	freeListPointer=freeListPointer->s.ptr);
+
+	// Join upper block
+	if(freeListPointer->s.ptr!=NULL && blockPointer + blockPointer->s.size == freeListPointer->s.ptr){
+		blockPointer->s.size += freeListPointer->s.ptr->s.size;
+		blockPointer->s.size = freeListPointer->s.ptr->s.ptr;
+	}else{
+		blockPointer->s.ptr = freeListPointer->s.ptr;
+	}
+
+	// Join lower block
+	if(freeListPointer + freeListPointer->s.size == blockPointer){
+		freeListPointer->s.size += blockPointer->s.size;
+		freeListPointer->s.ptr = blockPointer->s.ptr;
+	}else{
+		freeListPointer->s.ptr = blockPointer;
+	}
+
+}
---
+++ Kernel/memoryManager.h
@@ -0,0 +1,12 @@
+#ifndef MEM_MAN_H
+#define MEM_MAN_H
+
+
+
+
+#include<stddef.h>
+#define BEGIN_MEM 0x600000
+#define END_MEM 0xFFFFFFFFF
+
+
+#endif
