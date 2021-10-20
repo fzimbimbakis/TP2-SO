a9b0d158e8e9a9506812298c33df9d35eec43dff
bsquillari <bsquillari@itba.edu.ar>
2021-10-20 10:28:40 -0300

    Más espacio para el MM. Son 10 MB si no me equivoco.

--- Kernel/memoryManager.h
+++ Kernel/memoryManager.h
@@ -6,7 +6,7 @@
 
 #include<stddef.h>
 #define BEGIN_MEM (unsigned)0x600000
-#define END_MEM (unsigned)0x700000
+#define END_MEM (unsigned)0xFFFFFF
 void * alloc(unsigned nbytes);
 void free(void * block);
 unsigned * memoryInfoMM();
--- Userland/SampleCodeModule/testMM.h
+++ Userland/SampleCodeModule/testMM.h
@@ -9,5 +9,5 @@ void testmm2();
 void testmm3();
 void testmm0();
 #define BEGIN_MEM 0x600000
-#define END_MEM 0x700000
+#define END_MEM 0xFFFFFF
 #endif //TP2_SO_TESTMM_H
