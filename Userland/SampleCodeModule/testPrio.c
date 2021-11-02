13cc9386e62a0982c98db5aa448ab4833f0c545c
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2021-11-01 22:07:08 -0300

    Pequeños cambios

--- Userland/SampleCodeModule/testPrio.c
+++ Userland/SampleCodeModule/testPrio.c
@@ -8,7 +8,7 @@ uint32_t _getpid(){
 }
 
 uint64_t _create_process(void * name){
-  return newP((uint64_t)name, "testPrio");
+  return newP((uint64_t)name, "endlessPrio");
 }
 
 uint64_t _nice(uint64_t pid, uint64_t newPrio){
